d314620741af913f78cd5e7927835918e48fed57
AvaCado <eliserichardsson@gmail.com>
2022-02-19 11:51:35 -0500

    complete with Benny's help

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,5 +1,6 @@
 const chai = require('chai');
 const assert = chai.assert;
+const {describe, it} = require('mocha');
 
 const wordSearch = require('../wordsearch.js')
 
@@ -15,10 +16,10 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
-  });
+  });   
 
   it("should return true if the word is present", function() {
     const result = wordSearch([
@@ -31,8 +32,31 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
 
     assert.isTrue(result);
   });
+
+  it("should return 'No valid puzzle given.' when no puzzle array is provided.", function() {
+    const result = wordSearch([], 'SEINFELD');
+
+    assert.equal(result, 'No valid puzzle given.');
+  });
+
+  it("should return 'No word to look for.' when no word string is provided.", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], '');
+
+    assert.equal(result, 'No word to look for.');
+  });
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,33 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const { transpose }  = require("../../d2/transpose/matrices");
+
+const wordSearch = (puzzle, word) => { 
+  if (Array.isArray(puzzle) && puzzle.length > 0 && word) {  
+    const horizontalLines = puzzle.map(letters => letters.join(''));
+    for (let line of horizontalLines) {
+      if (line.includes(word)) {
+        return true;
+      }
     }
+
+    const verticalLines = transpose(puzzle).map(letters => letters.join(''));
+    for (let line of verticalLines) {
+      if (line.includes(word)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  return word ? "No valid puzzle given." : 'No word to look for.';
 }
 
-module.exports = wordSearch
+//Benny's adjustments:
+// const checkWord = (word) => (puzzle) => puzzle
+//   .map(letters => letters.join('').toLowerCase())
+//   .some(line => line.includes(word.toLowerCase()));
+
+//   const wordSearch = (puzzle= [''], word = '') => 
+//   [puzzle, transpose(puzzle)].some(checkWord(word))
+
+module.exports = wordSearch;
